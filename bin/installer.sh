fd221a11cbf22922e7ba3fc9c7adcac400f740e0
dzove855 <vehbo97@gmail.com>
2018-02-06 12:10:07 +0100

    Correct installer

--- bin/installer.sh
+++ bin/installer.sh
@@ -47,7 +47,7 @@ tmpDir="$(mktemp -d)"
 
 mkdir -p $tmpDir/usr/share/yosh
 
-cp -r $baseDir/{auth,func,lib} $tmpDir/usr/share/yosh
+cp -r $baseDir/{auth,func,lib,session} $tmpDir/usr/share/yosh
 cp -r $baseDir/autoloader.sh $tmpDir/usr/share/yosh/
 
 cp -r $baseDir/DEBIAN $tmpDir/
