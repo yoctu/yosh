3657776b6d15f1145f5abcc74ef2567dab5a3b76
dzove855 <v.dzogovic@flash-global.net>
2018-03-28 23:43:43 +0200

    Add url encode

--- func/base.sh
+++ func/base.sh
@@ -58,3 +58,16 @@ function url_decode ()
     
     echo -e "$value"
 }
+
+function url_encode() {
+    local _length="${#1}"
+    for (( _offset = 0 ; _offset < _length ; _offset++ )); do
+        _print_offset="${1:_offset:1}"
+        case "${_print_offset}" in
+            [a-zA-Z0-9.~_-]) printf "${_print_offset}" ;;
+            ' ') printf + ;;
+            *) printf '%%%X' "'${_print_offset}" ;;
+        esac
+    done
+}
+
