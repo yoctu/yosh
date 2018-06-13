df824a64aec16a7fa73d324af9a303c11a48fdf5
dzove855 <v.dzogovic@flash-global.net>
2018-06-13 18:41:29 +0200

    Add urlencode for GET

--- lib/http-read.sh
+++ lib/http-read.sh
@@ -63,7 +63,7 @@ function http::read::get ()
     for key in "${raw[@]}"
     do
         trim key
-        GET[${key%%=*}]="${key#*=}"
+        GET[$(urlencode -d "${key%%=*}")]="$(urlencde -d "${key#*=}")"
     done
     
     unset key
