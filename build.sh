aef9b132fa548f18b350da2ff16dc8f1cedd01fa
dzove855 <v.dzogovic@flash-global.net>
2018-06-28 11:37:14 +0200

    write build.sh

--- build.sh
+++ build.sh
@@ -44,9 +44,12 @@ oldPWD="$PWD"
 sudo apt-get install -y apt-transport-https
 wget -qO - https://ppa.yoctu.com/archive.key | sudo apt-key add -
 
+sudo curl -o /bin/jq -O -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+sudo chmod +x /bin/jq
+
 echo "deb https://ppa.yoctu.com/ all unstable" | sudo tee /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install yoctu-client-scripts
+sudo apt-get --nodeps install yoctu-client-scripts
 
 #git log --first-parent --pretty="format:  * %s (%aN, %aI)"
 
