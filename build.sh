2a954e0af11ecbaaa3d5650ae24d8e486f50d41a
dzove855 <v.dzogovic@flash-global.net>
2018-07-04 11:42:21 +0200

    Build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ oldPWD="$PWD"
 sudo apt-get update; sudo apt-get install -y apt-transport-https devscripts debianutils jq gridsite-clients
 wget -qO - https://ppa.yoctu.com/archive.key | sudo apt-key add -
 
-curl https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /tmp/jq
+curl -O -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /tmp/jq
 chmod +x /tmp/jq
 mv /tmp/jq /usr/bin/jq
 
