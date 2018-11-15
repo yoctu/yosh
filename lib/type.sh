ce71537f3e7b74543dcf6a1af83d35a088695547
dzove855 <v.dzogovic@flash-global.net>
2018-11-15 14:17:22 +0100

    Add type lib

---
+++ lib/type.sh
@@ -0,0 +1,42 @@
+# a litte lib of type checking and data checker
+type::function::exist(){
+    # multiple function are accepted
+
+    for _function in "$@"; do
+        if [[ $(type -t "$_function") == "function" ]]; then
+            return 0
+        else
+            return 1
+        fi
+    done
+}
+
+type::command::exist(){
+    # multiple commands are accepted
+    
+    for _command in "$@"; do
+        type "$_command" &>/dev/null
+    done
+}
+
+type::array::contains(){
+    # check if array contains value
+    
+    local key="$1"
+    local -n array="$2"
+
+    for value in "${array[@]}"; do
+        [[ "$key" == "$value" ]] && return 0
+    done
+
+    return 1
+}
+
+type::variable::set(){
+    # Check if varaibles are set
+
+    for value in "$@"; do
+        [[ -z "${!value}" ]] && return 1
+    done
+}
+
