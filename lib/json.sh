a64341b6653f84d39ed90937cd572293e89c5eca
dzove855 <v.dzogovic@flash-global.net>
2018-11-18 21:04:46 +0100

    Create recursive json creator

--- lib/json.sh
+++ lib/json.sh
@@ -1,57 +1,42 @@
-Json::set::prev(){
-    [[ -z "$1" ]] && return 1
+Json::create(){
+    local -n array="$1"
+    
+    local -a tmparray
+    local tmpvar
 
-    _json_array="$1"
-
-}
-
-Json::set::array(){
-    local _value
-
-    [[ -z "$1" ]] && return 1
-    [[ -z "$2" ]] && return 2
-
-    typeset -n _json_tmp_array="$2"
-    typeset -n _json_tmp_array_2="$1"
-
-    for key in "${_json_tmp_array[@]}"
-    do
-        _value+="\"$key\","
+    for key in "${!array[@]}"; do
+        IFS=":" read -a tmparray2 <<<"$key"
+        count="0"
+        for key_2 in ${tmparray2[@]}; do
+            tmpvar+="{ \"$key_2\": "
+            ((count++))
+        done
+        i="0"
+        tmpvar+=" \"${array[$key]}\""
+        until (( i == count )); do
+            tmpvar+="}"
+            ((i++))
+        done
+        tmparray+=("$tmpvar")
+        unset tmpvar
     done
 
-    _json_tmp_array_2[$2]="[${_value%,}]"
+    local tmpFile="$(mktemp)"
+    local tmpFile2="$(mktemp)"
+    count="0"
+    for entry in "${tmparray[@]}"; do
+        if ! (( count )); then
+            echo "$entry" >> $tmpFile
+        else
+            echo "$entry" > $tmpFile2
 
-}
+            output="$(jq -s -r '.[0] * .[1]' $tmpFile $tmpFile2)"
+            echo "$output" > $tmpFile
+        fi
+        ((count++))
+    done
 
-Json::set::next(){
-    [[ -z "$1" ]] && return 1
-    [[ -z "$2" ]] && return 2
-
-    typeset -n _json_tmp_array="$1"
-    _json_tmp_array[${2}]=$(json::create "$2")
-
-}
-
-Json::build::all(){
-    Json::create "$_json_array"
-}
-
-Json::create(){
-    # this function generate an json from an array
-    # jq using from FAQ mentioned by CharlesDuffy
-
-    typeset -n _json_tmp_array="$1"
-
-    for key in "${!_json_tmp_array[@]}"
-    do
-        printf '%s\0%s\0' "$key" "${_json_tmp_array[$key]}"
-    done | jq -c -Rs -S '
-                split("\u0000")
-                | . as $a
-                | reduce range(0; length/2) as $i 
-                ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})'    
-
-    unset _json_tmp_array
+    cat $tmpFile
 }
 
 Json::to::array(){
