e47d3a393854ae51c9480dbac1798ba257733cd8
dzove855 <v.dzogovic@flash-global.net>
2018-12-03 18:10:20 +0100

    rewrite json::create

--- lib/json.sh
+++ lib/json.sh
@@ -24,84 +24,33 @@ Json::create::simple(){
 }
 
 Json::create(){
-    # Argument should be an array
-    # to get a recursive json you can put in an array like this:
-    # array['key':'subkey']="value"
-
-    Type::array::is::assoc "$1" || return 1
     local -n array="$1"
- 
-    local -a tmparray
-    local tmpvar
-    local jsonarray
-    local countingRun
-    local count="0"
-    local i="0"
-    local tmpArrayVar
 
-    for key in "${!array[@]}" "END"; do
-        IFS=":" read -a tmparray2 <<<"$key"
+    Type::array::is::assoc "$1"
 
-        if [[ "$key" =~ .*:0$ ]]; then
-            jsonarray=1
-            tmpArrayVar="${array[$key]},"
-        elif [[ "$jsonarray" == 1 && "$key" =~ .*:[1-9]+$ ]]; then
-            tmpArrayVar+="${array[$key]},"
-            countingRun="1"
-        elif [[ "$jsonarray" == "1" && ! "$key" =~ .*:[0-9]+$ && "$key" == "END" ]]; then
-            jsonarray="0"
-            tmpvar+=$'$b | split(",")'
-            until (( i == count )); do
-                tmpvar+=" }"
-                ((i++))
-            done
-            tmparray+=("$(jq -n -c -R --arg b "${tmpArrayVar%,}" " $tmpvar")")
-            countingRun="0"
-            tmpvar=""
-            i=""
-            count=""
-            tmpArrayVar=""
-        fi
-
-        [[ "$key" == END ]] && break
-
-        for key_2 in "${tmparray2[@]}"; do
-            if [[ ! "$countingRun" == "1" ]] && [[ ! "$key_2" == "0" ]]; then
-                tmpvar+="{ \"$key_2\" : "
-                ((count++))
+    for key in "${!array[@]}"
+    do
+        IFS=':' read -ra subKeys <<< "$key"
+        for subKey in "${subKeys[@]}"; do
+            if [[ "$subKey" != "0" && "$jsonArray" != "1" ]]; then
+                echo -n "{ \"$subKey\" :" 
+                end+=" }"
+            elif [[ "$subKey" == "0" ]]; then
+                jsonArray="1"
+                echo -n "["
+                echo -n "\"${array[$key]}\","
+            elif [[ "$subKey" =~ ^.*:[0-9]+$ ]] && [[ "$jsonArray" == "1" ]]; then
+                echo -n "${${array[$key]}},"
+            elif [[ ! "$subKey" =~ ^.*:[0-9]+$ ]] && [[ "$jsonArray" == "1" ]]; then
+                unset jsonArray
+                echo -n "]"
+                echo "$end"
+                unset end
+                echo -n "{ \"$subKey\" :" 
             fi
         done
-        
-        [[ "$jsonarray" == 1 ]] && continue
-
-        tmpvar+='$b'
-        until (( i == count )); do
-            tmpvar+=" }"
-            ((i++))
-        done
-
-        tmparray+=("$(echo "${array[$key]//[$'\t\r\n']}" | jq -c -R ". as \$b | $tmpvar")")
-        count="0"
-        tmpvar=""
-        i="0"
-    done
-
-    local tmpFile="$(mktemp)"
-    local tmpFile2="$(mktemp)"
-    for entry in "${tmparray[@]}"; do
-        if ! [[ -s "$tmpFile" ]]; then
-            echo "${entry}" >> $tmpFile
-        else
-            echo "${entry}" > $tmpFile2
-            output="$(jq -c -Rs '(.|split("\n")[0]|fromjson) * (.|split("\n")[1]|fromjson)' "$tmpFile" "$tmpFile2")"
-            echo "${output}" > $tmpFile
-        fi
-    done
-
-    cat $tmpFile
-
-    # Cleanup tmpfiles
-    rm $tmpFile $tmpFile2
+        [[ "$jsonArray" != "1" ]] && { echo -n "\"${array[$key]}\""; echo "$end"; unset end; }
+    done #| jq --slurp 'reduce .[] as $item ({}; . * $item)'
 }
 
 Json::to::array(){
@@ -114,8 +63,7 @@ Json::to::array(){
 
     while read line; do
         [[ "$line" == @({|}) ]] && continue
-        [[ "$line" =~ ^\"(.*)\":.* ]] && key="${BASH_REMATCH[1]}"
-        [[ "$line" =~ .*:[[:space:]]\"(.*)\" ]] && value="${BASH_REMATCH[1]}"
+        [[ "$line" =~ ^\"(.*)\":[[:space:]]\"(.*)\" ]] && { key="${BASH_REMATCH[1]}"; value="${BASH_REMATCH[2]}"; }
         array[${key}]="${value}"
     done < <(echo "$json" | jq --arg delim ':' 'reduce (tostream|select(length==2)) as $i ({}; [$i[0][]|tostring] as $path_as_strings | ($path_as_strings|join($delim)) as $key | $i[1] as $value | .[$key] = $value )')
 }
