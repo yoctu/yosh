4da00ebd02de20f4f3e5cd11f222f18058faa8a6
dzove855 <v.dzogovic@flash-global.net>
2018-12-28 15:58:46 +0100

    Run google style guide ond autoloader.sh

--- autoloader.sh
+++ autoloader.sh
@@ -5,32 +5,27 @@ shopt -s expand_aliases
 
 # Auto Load all
 
-for file in /usr/share/yosh/{lib,func,auth,session,config}/*.sh
-do
+for file in /usr/share/yosh/{lib,func,auth,session,config}/*.sh; do
     source $file
 done
 
-for file in /usr/share/yosh/package/*/{lib,func,auth,session,config}/*.sh
-do
+for file in /usr/share/yosh/package/*/{lib,func,auth,session,config}/*.sh; do
     source $file
 done
 
 # Source custom lib's
-for file in ${DOCUMENT_ROOT%/}/../{lib,func,auth,session,config}/*.sh
-do
+for file in ${DOCUMENT_ROOT%/}/../{lib,func,auth,session,config}/*.sh; do
     source $file
 done
 
 
 # Packaging system
 # Lib Files for packaging system
-for file in ${DOCUMENT_ROOT%/}/../package/*/{lib,func,auth,session,config}/*.sh
-do
+for file in ${DOCUMENT_ROOT%/}/../package/*/{lib,func,auth,session,config}/*.sh; do
     source $file
 done
 
-for file in ${etc_conf_dir%/}/*.sh
-do
+for file in ${etc_conf_dir%/}/*.sh; do
     source $file
 done
 
