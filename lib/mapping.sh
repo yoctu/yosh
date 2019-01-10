15503bae72cb982891f1d7fe580652001f1f897a
Sofian Saada <sofian.saada@epitech.eu>
2019-01-10 22:40:47 +0100

    add functions and refactorisation

--- lib/mapping.sh
+++ lib/mapping.sh
@@ -4,9 +4,10 @@
 Mapping::get::get_key() {
     string="$1"
     
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]] # *1
+    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
     then
 	rematchKey="${BASH_REMATCH[1]}"
+	echo "rematchKey -- getKey == ${rematchKey}"
     else
 	echo "no match key"
     fi
@@ -15,9 +16,10 @@ Mapping::get::get_key() {
 Mapping::get::get_id() {
     string="$1"
 
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]] # *1
+    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
     then
 	rematchID="${BASH_REMATCH[2]}"
+	echo "rematchID -- getId == ${rematchID}"
     else
 	echo "no match id"
     fi 
@@ -26,88 +28,119 @@ Mapping::get::get_id() {
 Mapping::get::get_rematch() {
     string="$1"
 
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]] # *1
+    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
     then
 	rematch="${BASH_REMATCH[0]}"
+	echo "rematchRematch -- getRematch == ${rematch}"
     else
 	echo "no match rematch"
     fi 
 }
 
-Mapping::check::check_match() {    
-    [[ -z "$1" && -z "$2" ]] && echo "arg empty or null"
+
+Mapping::check::check_template() {
 
     string="$1"
-    template="$2"
-
+    
     if [[ "$template" =~ (.+)\{(.*)\}(.*) ]]
     then
 	route="${BASH_REMATCH[1]}"
 	id="${BASH_REMATCH[2]}"
 	end="${BASH_REMATCH[3]}"
-	
-	echo "check_match == array tmp"
-	declare -p arrayTmp BASH_REMATCH
-	echo "TEMPLATE check_match id == ${BASH_REMATCH[2]} && end == ${end}"
-	echo "TEMPLATE check_match == after bash_rematch"
+	Mapping::get::get_key $string
     else
-	echo "bad template" && return
-    fi
+        echo "bad template" && exit
+    fi 
+}
+
+Mapping::check::check_string() {
     if [[ "$string" =~ $route(.+)$end ]]
     then
-	echo "STRING route == ${route} && id == ${id} && end == ${end}"
-	arrayTmp["${id}"]="${BASH_REMATCH[1]}"
-	echo "STRING id == ${BASH_REMATCH[1]}"
-	echo "STRING id after arrayTmp stk == ${id} && arraytmp[id] == ${arrayTmp["$id"]}"
-	#arrayTmp["$route"]="${BASH_REMATCH[2]}"
+	return
     else
-	arrayTmp["$id"]=""
-	#arrayTmp["$route"]=""
 	echo "bad string" && exit
     fi
-#    declare -p arrayTmp
+}
+
+#Mapping::update::update_array() {
+
+#}
+
+Mapping::check::check_match() {    
+    [[ -z "$1" && -z "$2" ]] && echo "arg empty or null" && exit
+
+    string="$1"
+    template="$2"
+
+    if Mapping::check::check_template $template $string;
+    then
+	Mapping::check::check_string $string
+    else
+	echo "error bad template or bad string" && exit
+    fi
 }
 
 Mapping::parsing::parse_route() {
 
-    [[ -z "$1" && -z "$2" && -z "$3" ]] && echo "error arg is missing parse_route" && exit
+    [[ -z "$1" && -z "$2" ]] && echo "error arg is missing parse_route" && exit
     
     string="$1"
     template="$2"
-
-    #SSS : copy the array in arg in a local array and merge in the array in arg
-    #SSS : delete {} with the regex
+    arrayZ=("$3")
     
     declare -A arrayTmp
 
+    printf 'array in arg %s\n' "${arrayZ[@]}"
+
+    #SSS : delete {} with the regex #memo
+
     if Mapping::check::check_match $string $template;
     then
-	arrayTmp[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"
+	arrayTmp[${rematchKey}]=${id}
+	#	arrayTmp[${id}]=${id}
+	echo "test test ${arrayTmp[${rematchKey}]} && $rematchKey"
 
-#	echo "parsing route == key main BASH === ${BASH_REMATCH[1]}"
-#	echo "parsing route == id main BASH  === ${BASH_REMATCH[2]}"
-#	printf '%s\n' "key ==  yoctu -> ${arrayTmp['yoctu']}"
+#	for key in "${!arrayTmp[${key]]}"; do
+#	done
+	
+	#SSS : copy the array in arg in a local array and merge in the array in arg #memo
+	
+	#echo "parsing route == ${arrayTmp[${key}]}"
+	#printf '%s\n' "key ==  yoctu -> ${arrayTmp['yoctu']}"
+       
     else
 	arrayTmp['BASH_REMATCH[1]']=""
+	arrayTmp[${key}]=""
+	#arrayTmp[${id}]=""
+	echo "bad string" && exit
 	echo "parsing route == array[yoctu] = empty"
+		arrayTmp[${key}]="${id}"
+	#arrayTmp[${id}]="${id}"
+	# SSS : The if is just for get $key, i can do here, i just need to modif the regex
+	#SSS : idk if the key is yoctu and i update de value ($id) related to yoctu
+	#arrayTmp[${key}]=${id}
+	
+	#or if the key is the $id(2345) and i just update the key with the value from the $string 
+	#arrayTmp[${id}]=${id}
     fi
-#    declare -p arrayTmp
 }
 
 declare -A array['yoctu']="124"
+array['sofian']="12"
 
-Mapping::parsing::parse_route "/api/yoctu/{2345}/" "/api/yoctu/{2345}/foo" $array
+Mapping::parsing::parse_route "/api/yoctu/{2345}/foo" "/api/yoctu/{2345}/foo" "${array[@]}"
+
+#Mapping::get::get_key "/api/yoctu/{5678}/foo"
+#Mapping::get::get_id "/api/yoctu/{5678}/foo"
+#Mapping::get::get_rematch "/api/yoctu/{5678}/foo"
 
 alias mapping::get_key='Mapping::get::get_key'
 alias mapping::get_id='Mapping::get::get_id'
 alias mapping::get_rematch='Mapping::get::get_rematch'
 alias mapping::check_match='Mapping::check::check_match'
 alias mapping::parse_route='Mapping::parsing::parse_route'
+alias mapping::check_template='Mapping::check::check_template'
+alias mapping::check_string='Mapping::check::check_string'
 
 #rematchKeys="${BASH_REMATCH[1]} == KEY"
 #rematchIDs="${BASH_REMATCH[2]} == ID"
-
-# SSS : add a fourth arg ? it would be a regex
-# SSS : If nothing is found it doesn't match
-# SSS : Match all the integer in the string between {}
-# SSS : Regex *1 match 2345 && yoctu
