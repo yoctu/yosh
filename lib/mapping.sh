e298bd080c4f8d0267f9e25e35d5d052fcfd6ae2
Sofian Saada <sofian.saada@epitech.eu>
2019-01-15 19:16:22 +0100

    update the final array with the new value

--- lib/mapping.sh
+++ lib/mapping.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #source ./json.sh just a memo
+#bash array parse the value in alphabet order (ascii)
 
 Mapping::get::get_key() {
     string="$1"
@@ -63,7 +64,8 @@ Mapping::check::check_string() {
 }
 
 #Mapping::update::update_array() {
-
+#array
+#string = value
 #}
 
 Mapping::check::check_match() {    
@@ -82,57 +84,71 @@ Mapping::check::check_match() {
 
 Mapping::parsing::parse_route() {
 
-    [[ -z "$1" && -z "$2" ]] && echo "error arg is missing parse_route" && exit
+    [[ -z "$1" && -z "$2"  && -z "$3" ]] && echo "error arg is missing parse_route" && exit
     
     string="$1"
     template="$2"
-    arrayZ=("$3")
+    testArr=($3)
+    
+    #    local -n refArray=$3
+    eval "declare -A arrayFinal"="${3#*=}"
+    
+    declare -A assoArraytest
+#    assoArrayTest="${!3}"
     
     declare -A arrayTmp
+    echo "arrayFinal key ===== ${!arrayFinal[@]}"
+    echo "arrayFinal value ===== ${arrayFinal[@]}"
+    echo "arrayFinal value yoctu ===== ${arrayFinal['yoctu']}"
+    echo "arrayFinal value sofian ===== ${arrayFinal['sofian']}"
 
-    printf 'array in arg %s\n' "${arrayZ[@]}"
+    #SSS : Vehbo tell me doesn't use eval, but i don't know how to do without, or the array need to be a global
+    
+    echo "******************************************************"
+    echo "test print lenght from third arg ${#testArr[@]}"
 
     #SSS : delete {} with the regex #memo
 
+    for lenght in "${#finalArray[@]}"; do
+    
     if Mapping::check::check_match $string $template;
     then
 	arrayTmp[${rematchKey}]=${id}
-	#	arrayTmp[${id}]=${id}
-	echo "test test ${arrayTmp[${rematchKey}]} && $rematchKey"
+	#arrayTmp[${id}]=${id}
 
-#	for key in "${!arrayTmp[${key]]}"; do
-#	done
-	
-	#SSS : copy the array in arg in a local array and merge in the array in arg #memo
-	
-	#echo "parsing route == ${arrayTmp[${key}]}"
-	#printf '%s\n' "key ==  yoctu -> ${arrayTmp['yoctu']}"
-       
+	for key in "${!arrayTmp[@]}"; do
+	    echo "before modif value in arrayFinal ${arrayFinal[@]}"
+	    if [[ "${!arrayTmp[${key}]}" == "${!arrayFinal[${key}]}" ]]
+	    then
+		echo "before modif value in arrayFinal ${arrayFinal[${rematchKey}]}"
+		arrayFinal[${rematchKey}]="${arrayTmp[${rematchKey}]}"
+		echo "after modif value in arrayFinal ${arrayFinal[${rematchKey}]}"
+		echo "value of yoctu in arrayFinal ${arrayFinal['yoctu']}"
+	    else
+		echo "error parse_route *** if * * *"
+	    fi
+	done
     else
-	arrayTmp['BASH_REMATCH[1]']=""
 	arrayTmp[${key}]=""
 	#arrayTmp[${id}]=""
 	echo "bad string" && exit
 	echo "parsing route == array[yoctu] = empty"
-		arrayTmp[${key}]="${id}"
-	#arrayTmp[${id}]="${id}"
-	# SSS : The if is just for get $key, i can do here, i just need to modif the regex
+	arrayTmp[${key}]="${id}"
+	#arrayTmp[${id}]="${id}"	
+
 	#SSS : idk if the key is yoctu and i update de value ($id) related to yoctu
-	#arrayTmp[${key}]=${id}
-	
 	#or if the key is the $id(2345) and i just update the key with the value from the $string 
-	#arrayTmp[${id}]=${id}
     fi
+    done
 }
 
-declare -A array['yoctu']="124"
-array['sofian']="12"
+declare -A arrayAsso
 
-Mapping::parsing::parse_route "/api/yoctu/{2345}/foo" "/api/yoctu/{2345}/foo" "${array[@]}"
+arrayAsso=(['yoctu']='123' ['sofian']='456')
 
-#Mapping::get::get_key "/api/yoctu/{5678}/foo"
-#Mapping::get::get_id "/api/yoctu/{5678}/foo"
-#Mapping::get::get_rematch "/api/yoctu/{5678}/foo"
+echo "first place arrayAsso ${!arrayAsso[@]}"
+
+Mapping::parsing::parse_route "/api/yoctu/{12000}/foo" "/api/yoctu/{12000}/foo" "$(declare -p arrayAsso)"
 
 alias mapping::get_key='Mapping::get::get_key'
 alias mapping::get_id='Mapping::get::get_id'
@@ -141,6 +157,3 @@ alias mapping::check_match='Mapping::check::check_match'
 alias mapping::parse_route='Mapping::parsing::parse_route'
 alias mapping::check_template='Mapping::check::check_template'
 alias mapping::check_string='Mapping::check::check_string'
-
-#rematchKeys="${BASH_REMATCH[1]} == KEY"
-#rematchIDs="${BASH_REMATCH[2]} == ID"
