5fa1060305fd2b17a92e7b532bafc19f1174b980
Sofian Saada <sofian.saada@epitech.eu>
2019-01-17 17:51:57 +0100

    mapping update and clear

--- lib/mappingLib.sh
+++ lib/mappingLib.sh
@@ -4,12 +4,12 @@ Mapping::get::getKey() {
 
     [private] string="$1"
     [private] rematchKey
+
+    Type::variable::set $string $template || return 1
     
-    [[ -z "$string" ]] && exit
-    
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
-    then
+    if [[ "$string" =~ (.+)\{(.*)\}(.*) ]]; then
 	rematchKey="${BASH_REMATCH[1]}"
+	Type::variable::set $rematchKey || return 1
 	echo "$rematchKey"
 	return 0
     else
@@ -19,10 +19,12 @@ Mapping::get::getKey() {
 Mapping::get::getId() {
     [private] string="$1"
     [private:string] rematchId
+
+    Type::variable::set $string || return 1
     
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
-    then
+    if [[ "$string" =~ (.+)\{(.*)\}(.*) ]]; then
 	rematchId="${BASH_REMATCH[2]}"
+	Type::variable::set $rematchId || return 1
 	echo "$rematchId"
 	return 0
     else
@@ -33,15 +35,14 @@ Mapping::get::getRematch() {
     [private] string="$1"
     [string] rematch
 
-    if [[ "$string" =~ ([a-zA-Z]*.)/\{(.+)\} ]]
-    then
+    if [[ "$string" =~ (.+)\{(.*)\}(.*) ]]; then
         rematch="${BASH_REMATCH[0]}"
+	Type::variable::set $rematch || return 1
 	echo "$rematch"
         return 0
     else
         return 1
 }
-
 
 Mapping::check::checkMatch() {
     [private] string="$1"
@@ -49,14 +50,18 @@ Mapping::check::checkMatch() {
     [private] route
     [private] id
     [private] end
+    [private:assoc] array="$3"
+
+    Type::variable::set $string $template || return 1
+    Type::array::is::assoc "$3" || return 1
     
-    if [[ "$template" =~ (.+)\{(.*)\}(.*) ]]
-    then
+    if [[ "$template" =~ (.+)\{(.*)\}(.*) ]]; then
 	route="${BASH_REMATCH[1]}"
         id="${BASH_REMATCH[2]}"
         end="${BASH_REMATCH[3]}"
-	if [[ "$string" =~ $route(.+)$end ]]
-	then
+	if [[ "$string" =~ $route(.+)$end ]]; then
+	    array["$key"]="${BASH_REMATCH[1]}"
+	    echo "$array"
 	    return 0
 	else
 	    return 1
@@ -66,33 +71,10 @@ Mapping::check::checkMatch() {
     fi
 }
 
-Mapping::update::updateArray() {
-    [private] string="$1"
-    [private] template="$2"
-    [private:asso] array="$3"
-    [private] id
-    [private] key
-    
-    [[ -z "$1" && -z "$2"]] && echo "error parse_route" && return 1
-    Type::array::is::assoc "$3" || return 1
-
-    #boucle for mutiple key
-    if Mapping::check::checkMatch $string $template;
-    then
-	id=$(Mapping::get::getId $string)
-	key=$(Mapping::get::getKey $string)
-    
-	array["${key}"]="${id}"
-	echo "${array}"
-	return 0
-    else
-	array["${key}"]=""
-	return 1
-    fi
-}
 
 alias mapping::getKey='Mapping::get::getKey'
 alias mapping::getId='Mapping::get::getId'
 alias mapping::getRematch='Mapping::get::getRematch'
 alias mapping::checkMatch='Mapping::check::checkMatch'
-alias mapping::parseRoute='Mapping::parse::parseRoute'
+
+#je bouge ma voiture et je test
