170286fdbd6d9b87f0906d2d6754a18906d7f552
dzove855 <v.dzogovic@flash-global.net>
2019-02-18 14:26:59 +0100

    cleanup yosh-runner.sh

--- bin/yosh-runner.sh
+++ bin/yosh-runner.sh
@@ -53,10 +53,6 @@ DOCUMENT_ROOT="${DOCUMENT_ROOT%/*}"
 # Source the config file
 [[ -f "$_config_file" ]] && source $_config_file 
 
-# redirect stdout and stderr of function to file, to print after
-tmpStdout="$(mktemp)"
-tmpStderr="$(mktemp)"
-
 # Clean TMP file on exit
 trap "rm $tmpStdout; rm $tmpStderr" EXIT
 
